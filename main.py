07f02be3452587f2a567723597f81692faedf9a3
spwJxey <100385865+spwJxey@users.noreply.github.com>
2022-03-03 22:42:15 +0000

    I completed all 4 lab questions

---
+++ main.py
@@ -0,0 +1,27 @@
+def part1():
+  n = 42
+  tau = 3.14159 * 2
+  school = "Sault College"
+  print ("Part 1 is complete")
+  
+def part2():
+  #this is for part 2
+  Class = "CSD110"        
+  desired_grade = 100     
+  hot2 = "hothot"
+  pi = 3.14159
+  print ("Part 2 is complete")
+
+def part3():
+  #this is for part 3
+  h = "Hot"
+  print ("Feeling..." + h*3 + "!")
+
+def part4():
+  #this is for part 4
+  base = 3000    # in cm
+  height = 1000  # in cm
+  volume = (1/3 * (base * base) * height)
+  print("The volume of a square pyramid with base " + str(base) + "cm and height " + str(height) + "cm is " + str(volume) + "cm^3")
+
+part4()
